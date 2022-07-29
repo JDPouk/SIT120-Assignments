b6f912fdd62734f6f43f7aa93b3b1c4f17cdf793
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2022-07-29 15:04:53 +1000

    [week3] Update Tasks

--- week3/task3.js
+++ week3/task3.js
@@ -2,6 +2,12 @@ const now = new Date(Date.now());
 console.log('The current time is ');
 console.log(now.toDateString());
 
+console.log(now.getDay());
+console.log(now.getDate());
+console.log(now.getMonth());
+console.log(now.getFullYear());
+
+
 let newDate = new Date();
 setTimeout(() => {
     newDate = Date.now()
