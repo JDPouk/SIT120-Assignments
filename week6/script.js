31e13c10b18e332e82401b3aaf96d1498a6cb021
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2022-08-13 23:00:08 +1000

    [week6] Credit Task 3 – Dynamic options rendering using v-for

--- week6/script.js
+++ week6/script.js
@@ -14,3 +14,14 @@ var app2 = new Vue({
   }
 })
 
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    selected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ]
+  }
+})
