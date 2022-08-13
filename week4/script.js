02029b9c586c0e08328894fd58e56f7238a21944
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2022-08-13 20:55:26 +1000

    [week4] Credit Task 2 – Conditionals and Loops

--- week4/script.js
+++ week4/script.js
@@ -18,3 +18,14 @@ var app = new Vue({
       seen: true
     }
   })
+
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  })
