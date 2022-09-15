d735ea575805f3d1b85b089de107ab41802b7c46
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2022-09-15 16:14:22 +1000

    [Assessment3] Fix cards

--- week7/script.js
+++ week7/script.js
@@ -41,3 +41,22 @@ components: {
   'task-list' : taskListComponent
 }
 });
+
+Vue.component('base-checkbox', {
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    checked: Boolean
+  },
+  template: `
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      v-on:change="$emit('change', $event.target.checked)"
+    >
+  `
+})
+
+new Vue({ el: '#components-demo' })
