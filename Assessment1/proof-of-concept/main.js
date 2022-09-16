f05f54cad2a11b6a21ea252b3cac2506ea0a8178
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2022-09-16 16:48:12 +1000

    [Assignment3] Add More Concepts

--- Assessment1/proof-of-concept/main.js
+++ Assessment1/proof-of-concept/main.js
@@ -1,18 +1,47 @@
 let vue = new Vue({
   el: '#app2',
   data: {
-    message: 0
+    grossIncome: "",
+    otherIncome: "",
+  },
+  computed: {
+    result() {
+      if((this.grossIncome.length !== 0 ) ){ 
+        return parseInt(this.grossIncome)
+      }
+      if(this.otherIncome.length !== 0 )
+      {
+        return parseInt(this.otherIncome)
+      }
+      else{return parseInt(this.grossIncome) + parseInt(this.otherIncome)}
+    }
   }
 })
-
+  
  new Vue({
     el: '#app',
     data: {
-        todos: [
-            { title: 'assignment1', checked: true },
-            { title: 'assignment2', checked: false },
-            { title: 'assignment3', checked: false },
-        ]
+      components: {
+        ButtonCounter
+      }
     }
 })
 
+new Vue({
+  el: '#example-1',
+  data: {
+    show: true
+  }
+})
+
+Vue.mixin({
+  created: function showAlert()
+  {
+      alert("Message Sent")
+      var heading3 = document.getElementById("heading3");
+  }
+})
+
+
+const valueSelector = '[data-testid=stepper-value]'
+expect(result.find(valueSelector).text()).toContain('0')
