23a7a10cb45cca13b1db326be875c627fb2e6fd8
kingjohn130 <31618815+kingjohn130@users.noreply.github.com>
2022-09-17 22:12:18 +1000

    [Assignment3] javascript comments

--- Assessment1/proof-of-concept/main.js
+++ Assessment1/proof-of-concept/main.js
@@ -1,34 +1,35 @@
 let vue = new Vue({
+  // new Vue app
   el: '#app2',
   data: {
+    // props that cotains the income values entered by the user
     grossIncome: "",
     otherIncome: "",
   },
   computed: {
+    // returns the sum of the income values
     result() {
+     // checks to see if the value is an empty string
       if((this.grossIncome.length !== 0 ) ){ 
+        // returns only the icome value with a integer
         return parseInt(this.grossIncome)
       }
+      
       if(this.otherIncome.length !== 0 )
       {
+         // returns only the icome value with a integer
         return parseInt(this.otherIncome)
       }
+      // returns the sum of bother income types if they not empty
       else{return parseInt(this.grossIncome) + parseInt(this.otherIncome)}
     }
   }
 })
   
- new Vue({
-    el: '#app',
-    data: {
-      components: {
-        ButtonCounter
-      }
-    }
-})
 
+ // returns the sum of bother income types if they not empty
 new Vue({
-  el: '#example-1',
+  el: '#transition_status',
   data: {
     show: true
   }
@@ -37,11 +38,13 @@ new Vue({
 Vue.mixin({
   created: function showAlert()
   {
+      // alerts when a input is clicked
+
       alert("Message Sent")
       var heading3 = document.getElementById("heading3");
   }
 })
 
-
+ // Testcase will be place here to check the value of a string
 const valueSelector = '[data-testid=stepper-value]'
 expect(result.find(valueSelector).text()).toContain('0')
